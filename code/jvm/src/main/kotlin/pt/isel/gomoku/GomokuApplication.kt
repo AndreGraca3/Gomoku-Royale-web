60fc2dae390249354cdb1d84ddc5d024bb50616f
ISEL-47224-23DA <sindrome.gracinha@gmail.com>
2023-09-22 19:08:28 +0100

    first commit

---
+++ code/jvm/src/main/kotlin/pt/isel/gomoku/GomokuApplication.kt
@@ -0,0 +1,11 @@
+package pt.isel.gomoku
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GomokuApplication
+
+fun main(args: Array<String>) {
+	runApplication<GomokuApplication>(*args)
+}
